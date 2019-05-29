ab166f7d1a6363e51e270bc36d8f3cc92d5addc5
Tomasz Wyrwa <wyrwa.t@gmail.com>
2019-05-29 10:28:17 +0200

    copy files

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  pageDots: false
+});
+
+var buttonGroup = document.querySelector('.button-group');
+var buttons = buttonGroup.querySelectorAll('.button');
+buttons = fizzyUIUtils.makeArray( buttons );
+
+buttonGroup.addEventListener( 'click', function( event ) {
+  flkty.select(0);
+});
