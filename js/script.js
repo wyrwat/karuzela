cc5493a0c47d181787d89b7cd0751aa42fa59679
Tomasz Wyrwa <wyrwa.t@gmail.com>
2019-06-06 13:25:09 +0200

    Add scroll

--- js/script.js
+++ js/script.js
@@ -15,12 +15,19 @@
   results.insertAdjacentHTML('beforeend', carouselList);
 })();
 
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
   contain: true,
   pageDots: false
+});
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
 });
 
 var buttonGroup = document.querySelector('.button-group');
