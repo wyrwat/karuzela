ec087fdb63368b47f49bd1852e758fb8a5801835
Tomasz Wyrwa <wyrwa.t@gmail.com>
2019-06-06 18:47:03 +0200

    Add markers

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
-
 (function(){
+var parseCarousel = function(map){
   var results = document.querySelector(".main-carousel");
   var carouselTemplate = document.getElementById('template-carousel').innerHTML;
   Mustache.parse(carouselTemplate);
@@ -8,38 +8,44 @@
 
   for( var i = 0; i < carouselData.length; i++) {
     var objectData = Object.assign(carouselData[i], { index: i+1 });
+    new google.maps.Marker(
+      {position: carouselData[i].coords, map:map}
+    )
     carouselItems += Mustache.render(carouselTemplate, objectData); 
   }
 
   var carouselList = Mustache.render(carouselItems);
   results.insertAdjacentHTML('beforeend', carouselList);
-})();
-
-
-window.initMap = function(){
-    // The location of Uluru
-    
-    var uluru = {lat: 35.718, lng: 139.771};
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
-  
 }
 
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true,
-  pageDots: false
-});
+var initSlider = function(){
+  var elem = document.querySelector('.main-carousel');
+  var flkty = new Flickity( elem, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    pageDots: false
+  });
 
-var buttonGroup = document.querySelector('.button-group');
-var buttons = buttonGroup.querySelectorAll('.button');
-buttons = fizzyUIUtils.makeArray( buttons );
+  var buttonGroup = document.querySelector('.button-group');
+  var buttons = buttonGroup.querySelectorAll('.button');
+  buttons = fizzyUIUtils.makeArray( buttons );
 
-buttonGroup.addEventListener( 'click', function( event ) {
-  flkty.select(0);
-});
+  buttonGroup.addEventListener( 'click', function( event ) {
+    flkty.select(0);
+  });
+}
+
+var initMap = function(){
+    var uluru = {lat: 35.718, lng: 139.771};
+    var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 2, center: uluru}
+    );
+    parseCarousel(map);
+    initSlider();
+    var marker = new google.maps.Marker({position: uluru, map: map});
+}
+
+
+  initMap();
+}());
